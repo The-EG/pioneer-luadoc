1011c99f7118b668b36ea1b6d5fd82608525481e
Commander Jameson <CmdrJameson@pioneerspacesim.net>
2021-01-02 02:40:04 +0000

    Update Lua Docs (Update pioneer submodule)

--- docs/lua/classes/Lua/HyperspaceCloud-ToolTips.js
+++ docs/lua/classes/Lua/HyperspaceCloud-ToolTips.js
@@ -1,1 +1,1 @@
-﻿NDContentPage.OnToolTipsLoaded({14:"<div class=\"NDToolTip TClass LLua\"><div class=\"TTSummary\">Class representing a ship. Inherits from Body.</div></div>",346:"<div class=\"NDToolTip TClass LLua\"><div class=\"TTSummary\">Class represents a physical body.</div></div>"});
+﻿NDContentPage.OnToolTipsLoaded({14:"<div class=\"NDToolTip TClass LLua\"><div class=\"TTSummary\">Class representing a ship. Inherits from Body.</div></div>",349:"<div class=\"NDToolTip TClass LLua\"><div class=\"TTSummary\">Class represents a physical body.</div></div>"});
--- docs/lua/classes/Lua/ShipDef-ToolTips.js
+++ docs/lua/classes/Lua/ShipDef-ToolTips.js
@@ -1,1 +1,1 @@
-﻿NDContentPage.OnToolTipsLoaded({672:"<div class=\"NDToolTip TConstant LLua\"><div class=\"TTSummary\">Thruster types. Used by &lt;ShipType.GetLinearThrust&gt;. The name corresponds to the direction the ship moves when the thruster is fired.</div></div>"});
+﻿NDContentPage.OnToolTipsLoaded({603:"<div class=\"NDToolTip TConstant LLua\"><div class=\"TTSummary\">Thruster types. Used by &lt;ShipType.GetLinearThrust&gt;. The name corresponds to the direction the ship moves when the thruster is fired.</div></div>"});
